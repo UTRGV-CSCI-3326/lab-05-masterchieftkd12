0b3ed8e71444b9e43fc0059c094d67a5a71bbef7
masterchieftkd12 <masterchieftkd12@gmail.com>
2025-01-29 23:21:30 +0000

    hi

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.print("What is your name: ");
+        name = scanner.nextLine();
+        
+        
+        int num;
+        System.out.print("what is your age: ");
+        num = scanner.nextInt();
+        
+        int num1;
+        System.out.print("what is your weight: ");
+        
+        num1 = scanner.nextInt();
+        
+        Boolean bool;
+        System.out.print("Do you smoke (true or false) : ");
+        
+        bool = scanner.nextBoolean();
+        
+        System.out.print(name + " " + num + " " + num1 + " " + bool);
+    }
+}
